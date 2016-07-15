ec47757ddc0729c1736cefe147ec753ef571b165
Ryan Quant <rquant@icloud.com>
2016-07-14 20:15:30 -0600

    Ensure main script isn't executed until document is ready

--- public/js/main.js
+++ public/js/main.js
@@ -1,101 +1,105 @@
 /*
  *loads all other required scripts and prepares the game for initialization
  */
+$(document).ready(function() {
 
-    (function (mainGame) {
+  (function (mainGame) {
 
-      //setup the main canvas used for the game
-      var canvas = $("#main-canvas")[0];
-      canvas.width = STAGE_WIDTH;
-      canvas.height = STAGE_HEIGHT;
+    //setup the main canvas used for the game
+    var canvas = $("#main-canvas")[0];
+    canvas.width = STAGE_WIDTH;
+    canvas.height = STAGE_HEIGHT;
 
-      var context = canvas.getContext("2d");
-      context.fillStyle = BACKGROUND_COLOR;
-      context.fillRect(0,0, canvas.width, canvas.height);
-      mainGame.stage = new createjs.Stage("main-canvas");
+    var context = canvas.getContext("2d");
+    context.fillStyle = BACKGROUND_COLOR;
+    context.fillRect(0,0, canvas.width, canvas.height);
+    mainGame.stage = new createjs.Stage("main-canvas");
 
-      debugCanvas = $("#debugCanvas")[0]; //seperate convas for debugging box2d simulation
+    debugCanvas = $("#debugCanvas")[0]; //seperate convas for debugging box2d simulation
 
-      //set up loading screen
-      var loadProgressContainer = new createjs.Container();
+    //set up loading screen
+    var loadProgressContainer = new createjs.Container();
 
-      var loadProgressLabel = new createjs.Text("Loading", "30px Avenir", "#FFFFFF");
-      loadProgressLabel.lineWidth = 200;
-      loadProgressLabel.textAlign = "center";
-      loadProgressLabel.x = STAGE_WIDTH/2;
-      loadProgressLabel.y = 0;
-      loadProgressContainer.addChild(loadProgressLabel);
+    var loadProgressLabel = new createjs.Text(
+      "Loading", "30px Avenir", "#FFFFFF"
+    );
+    loadProgressLabel.lineWidth = 200;
+    loadProgressLabel.textAlign = "center";
+    loadProgressLabel.x = STAGE_WIDTH/2;
+    loadProgressLabel.y = 0;
+    loadProgressContainer.addChild(loadProgressLabel);
 
-      var loadingBarContainer = new createjs.Container();
-      var loadingBarHeight = 20;
-      var loadingBarWidth = 400;
+    var loadingBarContainer = new createjs.Container();
+    var loadingBarHeight = 20;
+    var loadingBarWidth = 400;
 
-      var loadingBar = new createjs.Shape();
-      loadingBar.graphics.beginFill("#FFFFFF").drawRect(0, 0, 1, loadingBarHeight).endFill();
-      var frame = new createjs.Shape();
-      frame.graphics.setStrokeStyle(2).beginStroke("#FFFFFF").drawRoundRect(0, 0, loadingBarWidth, loadingBarHeight, 2);
+    var loadingBar = new createjs.Shape();
+    loadingBar.graphics.beginFill("#FFFFFF").drawRect(0, 0, 1, loadingBarHeight).endFill();
+    var frame = new createjs.Shape();
+    frame.graphics.setStrokeStyle(2).beginStroke("#FFFFFF").drawRoundRect(0, 0, loadingBarWidth, loadingBarHeight, 2);
 
 
-      loadingBarContainer.addChild(frame, loadingBar);
+    loadingBarContainer.addChild(frame, loadingBar);
 
-      loadingBarContainer.x = Math.round(STAGE_WIDTH/2 - loadingBarWidth/2);
-      loadingBarContainer.y = 50;
+    loadingBarContainer.x = Math.round(STAGE_WIDTH/2 - loadingBarWidth/2);
+    loadingBarContainer.y = 50;
 
-      var padding = 50; //the space between the text and the loading bar
-      var loadProgressContainerHeight = loadingBarHeight + padding + 30;
-      loadProgressContainer.addChild(loadingBarContainer);
-      loadProgressContainer.y = STAGE_HEIGHT/2 - loadProgressContainerHeight/2;
+    var padding = 50; //the space between the text and the loading bar
+    var loadProgressContainerHeight = loadingBarHeight + padding + 30;
+    loadProgressContainer.addChild(loadingBarContainer);
+    loadProgressContainer.y = STAGE_HEIGHT/2 - loadProgressContainerHeight/2;
 
-      mainGame.stage.addChild(loadProgressContainer);
-      mainGame.stage.update();
+    mainGame.stage.addChild(loadProgressContainer);
+    mainGame.stage.update();
 
-      //the preloader is responsible for loading resources and its progress can be used to update the loading bar
-      var preloader = new createjs.LoadQueue(false);
-      preloader.installPlugin(createjs.Sound);
-      preloader.addEventListener("progress", handleProgress);
-      preloader.addEventListener("complete", handleComplete);
-      preloader.loadManifest([
-          {src:"js/lib/GlowFilter.js"},
-          {src:"js/util.js"},
-          {src:"js/modules/box2dModule.js"},
+    //the preloader is responsible for loading resources and its progress can be used to update the loading bar
+    var preloader = new createjs.LoadQueue(false);
+    preloader.installPlugin(createjs.Sound);
+    preloader.addEventListener("progress", handleProgress);
+    preloader.addEventListener("complete", handleComplete);
+    preloader.loadManifest([
+        {src:"js/lib/GlowFilter.js"},
+        {src:"js/util.js"},
+        {src:"js/modules/box2dModule.js"},
 
-          {src:"js/game_assets/Star.js"},
-          {src:"js/game_assets/Planet.js"},
-          {src:"js/game_assets/Player.js"},
-          {src:"js/setup_game.js"}
-      ]);
+        {src:"js/game_assets/Star.js"},
+        {src:"js/game_assets/Planet.js"},
+        {src:"js/game_assets/Player.js"},
+        {src:"js/setup_game.js"}
+    ]);
 
-      function handleProgress() {
-          loadingBar.scaleX = preloader.progress * loadingBarWidth;
-          mainGame.stage.update();
-      }
+    function handleProgress() {
+        loadingBar.scaleX = preloader.progress * loadingBarWidth;
+        mainGame.stage.update();
+    }
 
-      //all resources loaded, begin the game introduction
-      function handleComplete() {
-          mainGame.stage.enableMouseOver(2);
-          mainGame.stage.removeChild(loadProgressContainer);
-          mainGame.stage.addChild(mainGame.containers.background);
+    //all resources loaded, begin the game introduction
+    function handleComplete() {
+        mainGame.stage.enableMouseOver(2);
+        mainGame.stage.removeChild(loadProgressContainer);
+        mainGame.stage.addChild(mainGame.containers.background);
 
-          createjs.Ticker.setFPS(30);
-          createjs.Ticker.setRAF = true;
-          createjs.Ticker.addEventListener("tick", backgroundTick);
-          createjs.Sound.play("soundtrack", {loop: -1});
-      }
+        createjs.Ticker.setFPS(30);
+        createjs.Ticker.setRAF = true;
+        createjs.Ticker.addEventListener("tick", backgroundTick);
+        createjs.Sound.play("soundtrack", {loop: -1});
+    }
 
-      //runs the background animation. updated on every frame
-      function backgroundTick() {
-          //run star field animation
-          for(var i=0; i < STAR_COUNT; i++) {
-              var s = mainGame.containers.stars.children[i];
-              if (s.y>=STAGE_HEIGHT) {
-                  s.x = Math.floor(Math.random()*STAGE_WIDTH);
-                  s.y = 0;
-              }
-              s.tick();
-          }
-          spacebounce.mainGame.stage.update();
-      }
+    //runs the background animation. updated on every frame
+    function backgroundTick() {
+        //run star field animation
+        for(var i=0; i < STAR_COUNT; i++) {
+            var s = mainGame.containers.stars.children[i];
+            if (s.y>=STAGE_HEIGHT) {
+                s.x = Math.floor(Math.random()*STAGE_WIDTH);
+                s.y = 0;
+            }
+            s.tick();
+        }
+        spacebounce.mainGame.stage.update();
+    }
   })(spacebounce.mainGame || {});
+});
 
 var orbDelayCounter = 0;
 var timeToRescue = FPS * TIME_TO_RESCUE;
