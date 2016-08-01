f0b35edc7a48425e956f0f8ce8df0baf1a836506
Ryan Quant <rquant@icloud.com>
2016-08-01 13:52:09 -0600

    Improve Button class abstraction for  MenuButton inheritence

--- public/js/classes/interface/Button.js
+++ public/js/classes/interface/Button.js
@@ -3,8 +3,8 @@
  */
 (function (spacebounce) {
 
-    function Button(text, width, height) {
-        this.initialize(text, width, height);
+    function Button(width, height) {
+        this.initialize(width, height);
     }
 
         var p = Button.prototype = new createjs.Container();
@@ -17,22 +17,19 @@
         p.label;
         p.Container_initialize = p.initialize;
 
-        p.initialize = function(text, width, height) {
-
+        p.initialize = function(width, height) {
           this.Container_initialize();
           this.width = width;
           this.height = height;
-          this.text = text;
+
           var underlay = new createjs.Shape(); //this is a transparent underlay so the button will detect click events any where in the container
           underlay.graphics.beginFill("#000").drawRoundRect(0, 0 , width, height, 2).endFill();
+
           var frame = new createjs.Shape();
-          frame.graphics.setStrokeStyle(2).beginStroke("#FFFFFF").drawRoundRect(0, 0 , width, height, 2).endFill();
-          var label = new createjs.Text(text, "20px Avenir", "#FFFFFF");
-          label.textAlign = "center";
-          label.textBaseline = "middle";
-          //offset the label to be in the center of the button
-          label.x = width/2;
-          label.y = height/2;
+          frame.graphics.setStrokeStyle(2).beginStroke("#FFFFFF").drawRoundRect(
+            0, 0 , width, height, 2
+          ).endFill();
+          var label = new createjs.Container();
 
           this.underlay = underlay;
           this.frame = frame;
--- public/js/classes/interface/MenuButton.js
+++ public/js/classes/interface/MenuButton.js
@@ -12,8 +12,17 @@
       p.Button_initialize = p.initialize;
 
       p.initialize = function(text) {
-          this.Button_initialize(text, MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
+          this.Button_initialize(MENU_BUTTON_WIDTH, MENU_BUTTON_HEIGHT);
           this.underlay.alpha = 0.7;
+
+          var textWrapper = new createjs.Text(text, "20px Avenir", "#FFFFFF");
+          textWrapper.textAlign = "center";
+          textWrapper.textBaseline = "middle";
+          //offset the textWrapper to be in the center of the button
+          textWrapper.x = this.width/2;
+          textWrapper.y = this.height/2;
+
+          this.label.addChild(textWrapper);
       }
 
     spacebounce.MenuButton = MenuButton;
